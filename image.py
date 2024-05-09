a410adf2fdcd1e1936f40e1235fdfcdb2ed6e2ac
KARTHIKEYASHARMA672 <148982048+KARTHIKEYASHARMA672@users.noreply.github.com>
2024-05-09 16:43:02 +0530

    Update image.py

--- image.py
+++ image.py
@@ -1,4 +1,4 @@
-### Image recognition
+### Health Management APP
 from dotenv import load_dotenv
 
 load_dotenv() ## load all the environment variables
@@ -35,9 +35,9 @@ def input_image_setup(uploaded_file):
     
 ##initialize our streamlit app
 
-st.set_page_config(page_title="Image recognition")
+st.set_page_config(page_title="Gemini Health App")
 
-st.header("Image recognition")
+st.header("Gemini Health App")
 input=st.text_input("Input Prompt: ",key="input")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 image=""   
@@ -46,23 +46,9 @@ if uploaded_file is not None:
     st.image(image, caption="Uploaded Image.", use_column_width=True)
 
 
-submit=st.button("Tell me the Details")
+submit=st.button("Tell me the total calories")
 
 input_prompt="""
-You are an expert in mobiles and electronics and you need to identify the mobile and its parts
-and need to define the parts and explain about the parts like battery,camera ,sound system, display,
-ram , processor, 4g or 5g .
-out put should br like the below moentioned format 
-                  FONT SHOULD TIMES NEW ROMAN
-                   list its parts one by one like bulletin points
-                   explain about its parts in seperatley with minimum 100 words
-                   who manufactured that ,machine (200 WORDS)
-                   say about that manufacturing company, THEIR AIM ETC ( 150 WORDS)
-    the above given should be made properly , with good english
-    
-
-    ----
-    ----
 You are an expert in nutritionist where you need to see the food items from the image
                and calculate the total calories, also provide the details of every food items with calories intake
                is below format
@@ -71,7 +57,89 @@ You are an expert in nutritionist where you need to see the food items from the 
                2. Item 2 - no of calories
                ----
                ----
- You are an expert cook and you know to make every dish in the world. detect the food item from the image. then give a detailed recipie in the given format(give ingredieants and steps in bullet style:
+
+You are an expert in recognising people, where you need to see that person recognise him/her and tell everything about him/her like there full name about there birth place & date also with death date & place
+in addition to there achievements, about there educational details in the below format.
+
+               1. Name - name of that personality with short description about him
+               2. Place of Birth - place of birth with birth date
+               3. Death - place and date of death
+               4. Education - write about there educational background
+               5. Family - write little information about there family 
+               6. Achievements - write about there achievements like prices, inventions, discoveries they did
+               ----
+               ----
+
+
+You are an expert in finding the cartoons from the images and telling the name of the cartoon along with the show
+            
+
+               1. Name - Name of the cartoon
+               2. Show name - Name of the show
+               3. Creater - Name of the maker
+               4. Fun Fact - Interesting fact about the cartoon
+               ----
+               ----
+
+You are an animal expert and you have to detect whatever animal from the image, response in the following format
+
+Name : <name of animal>
+Species : <scientific name>
+Habitat: <habitat of animal>
+Type: <wether animal is carnivorous, omnivorous, or herbivore>
+Status : <if animal is exint or not>
+Origin : <nationality of region of animal>
+About : <brief description of animal>
+
+
+You are an expert in machines or electronics and you need to identify the machines and electronics parts
+and need to define the parts and explain about the parts and how they work how the machine is made .
+give real life application , about it and say how it is evolved 
+
+
+output should be like the below mentioned thing 
+
+                  FONT SHOULD TIMES NEW ROMAN 
+                  
+                  the identified name of the machine ( itshould be bold bigger in text in old london font it should be bigger than other text this is title)
+                  
+                  list its parts one by one like bulletin points
+                  
+                  explain about its parts in seperatley with minimuym 200 words
+
+            
+                  say how that identified machine works (470 WORDS)
+                  
+                  how the identified machine is made ( minimum350 word)
+                  
+                  how the identified machine has evolved (350 WORDS)
+                
+                 then  say its real life application , where it is usedin bulletin points minimum 18 points or application
+                  
+                  who invented the machine (300 WORDS)
+                  
+                  who manufactured that ,machine (375 WORDS)
+                  
+                  say about that manufacturing company, THEIR AIM ETC ( 450 WORDS)
+                  
+                  
+       the above given shouldbe made properly , with good english .
+                  
+
+                  
+                                    
+
+               ----
+               ----
+
+
+Give me the details about chemical and preservatives in the junk food 
+for examples tell about the safety preservative level and chemical level can be consumed,tell about the percentage of chemicals and preservatives added 
+
+               ----
+               ----
+
+You are an expert cook and you know to make every dish in the world. detect the food item from the image. then give a detailed recipie in the given format(give ingredieants and steps in bullet style:
 Food recognized : <name of food> 
 Origin : <nationality/region>
 
@@ -83,24 +151,6 @@ Steps to prepare   step 1 : <step 1>
                       ...
 
 Your <food name> is ready
-
-You are an expert  identifing  the companies and need to define the details of the company
-like Founder,Business Diversification,Key Companies, Global Presence,Social Responsibility,
-Leadership and Governance,Innovation and sutainability,acquisitions. provided
-out put should br like the below mentioned format.
-                  FONT SHOULD TIMES NEW ROMAN
-                   list its parts one by one like bullet in points
-                   explain about  each seperatley with minimum 100 words
-    the above given should be made properly , with good english
-
-    ----
-    ----
-
-you are expert in human gender identification you need to identify the gender of the person
-    ----
-    ----
-
-    
 
 
 """
