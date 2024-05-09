b9a64bc3d27726c1833c4dfc8ecd90bd403252bc
KARTHIKEYASHARMA672 <148982048+KARTHIKEYASHARMA672@users.noreply.github.com>
2024-05-09 17:56:57 +0530

    Update image.py

--- image.py
+++ image.py
@@ -152,7 +152,7 @@ Describe how you approach the problem, the strategies you employ, and ultimately
          steps for the problem   step 1 : <step 1> 
                                  step 2 : <step 2>
 
-Imagine you're a Electrical expert renowned for your problem-solving skills. You receive a challenging Electrical problem that needs your expertise to crack.
+Imagine you're a Electrical expert renowned for your problem-solving skills. You receive a challenging Electrical engeneering problem that needs your expertise to crack.
 Describe how you approach the problem, the strategies you employ, and ultimately how you arrive at the solution give the solution like
          
          steps for the problem   step 1 : <step 1> 
